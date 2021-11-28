4b6b188da85acf3f0b11bc404322c550d4e29308
Oleg Vaskevich <oleg.vaskevich.it@gmail.com>
2021-11-28 14:18:18 +0300

    feat: implement custom hook useGetProducts

--- src/components/productsPage/productsSection/productsSection.tsx
+++ src/components/productsPage/productsSection/productsSection.tsx
@@ -1,26 +1,12 @@
-import { useEffect } from "react";
-import { useParams } from "react-router-dom";
-import { useDispatch } from "react-redux";
-import useAppSelector from "@/redux/hooks/useAppSelector";
-import SAGA_ACTIONS from "@/redux/sagas/sagaActions/sagaActions";
 import Container from "@/elements/container/container";
 import GameCard from "@/elements/gameCard/gameCard";
 import GameCardsContainer from "@/elements/gameCardsContainer/gameCardsContainer";
 import SearchBar from "@/elements/searchbar/searchbar";
 import useSkeleton from "@/hooks/useSkeleton";
-
-interface IRouterParams {
-  slug: string;
-}
+import useGetProducts from "@/hooks/useGetProducts";
 
 const ProductsSection = (): JSX.Element => {
-  const dispatch = useDispatch();
-  const { products, filter, isProductsLoading } = useAppSelector((state) => state.PRODUCTS);
-  const { slug } = useParams<IRouterParams>();
-
-  useEffect(() => {
-    dispatch({ type: SAGA_ACTIONS.GET_PRODUCTS, payload: { ...filter, category: slug || null } });
-  }, [filter, slug]);
+  const { products, isProductsLoading } = useGetProducts();
 
   return (
     <section className="section__products">
